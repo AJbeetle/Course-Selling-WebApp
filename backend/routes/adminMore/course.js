b7a75cb21101c509f2778b37d890b4ce3b96c552
aayushiJoshi <aayushijoshi9910@gmail.com>
2025-03-14 23:33:54 +0530

    adminRoute for adding content into course

--- backend/routes/adminMore/course.js
+++ backend/routes/adminMore/course.js
@@ -1,12 +1,16 @@
 const {Router} = require("express");
 const courseRouter = Router();
-const {handleContentUpdates, handleAboutUpdates} = require("../../controllers/admin");
+const {handleContentUpdates, handleAboutUpdates, handleNewContentAddition} = require("../../controllers/admin");
 
 // admin can make changes in the content of the course : like adding lectures, notes etc.
 courseRouter.patch("/content",handleContentUpdates);
 
 // admin can make changes to the information page of the course i.e. not internal content but just outer layer of it like : title, price or description is updated
-courseRouter.patch("/about",handleAboutUpdates);
+courseRouter.patch("/about",handleAboutUpdates);   //_DONE_
+
+
+// adding new content to the course
+courseRouter.post("/content",handleNewContentAddition);
 
 
 
