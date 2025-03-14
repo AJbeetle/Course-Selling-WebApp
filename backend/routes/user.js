7637b3b1d2c7e1bed062617d68c5392d2993696d
aayushiJoshi <aayushijoshi9910@gmail.com>
2025-03-14 23:32:29 +0530

    more userRoutes added

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,6 @@
 const {Router} = require("express");
 const userRouter = Router();
-const {handleDashboard, handleUserCourses, handleUserSignup, handleUserLogin } = require("../controllers/user");
+const {handleDashboard, handleUserCourses, handleUserSignup, handleUserLogin, handleUserCourseContent, handleUserCoursesAll } = require("../controllers/user");
 const {authenticate} = require("../middlewares/authenticate");
 const {courseRouter} = require("./userMore/course");
 const {purchaseRouter} = require("./userMore/purchase");
@@ -20,12 +20,15 @@ userRouter.use((req, res, next)=>{
 
 userRouter.get("/dashboard", handleDashboard);
 userRouter.get("/myCourses", handleUserCourses);
+userRouter.get("/myCoursesAll", handleUserCoursesAll);
+userRouter.get("/myCourses/content", handleUserCourseContent);
 
 // there are multiple things that can be happening in courseUpdate section of user : either they are enrolling or unenrolling , second thing can be they their progress updates in each course
 userRouter.use("/updateCourse", courseRouter);
 
 
-userRouter.use("/buyCourse/:courseName", purchaseRouter);
+//user sends course name or courseId in body
+userRouter.use("/buyCourse/", purchaseRouter);
 
 
 
