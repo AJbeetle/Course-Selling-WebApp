fa40de7e4744001f262e5b9b371b5ab5bb07352c
aayushiJoshi <aayushijoshi9910@gmail.com>
2025-03-14 23:31:38 +0530

    more adminRoutes added

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -4,9 +4,11 @@ const {
         handleAdminSignup, 
         handleAdminLogin, 
         handleAdminCourseAdd, 
-        handleAdminCourses, 
+        handleAdminCourses,
+        handleAdminCourseContent, 
         handleAdminCourseDelete,
-        handleAdminDashboard
+        handleAdminCourseSTATUS,
+        handleAdminDashboard,
        } = require("../controllers/admin");
 
 const {authenticate} = require("../middlewares/authenticate");
@@ -18,21 +20,25 @@ const jwt = process.env.JWT_ADMIN;
 const {courseRouter} = require("./adminMore/course");
 
 
-adminRouter.post("/signup", handleAdminSignup);
-adminRouter.post("/login", handleAdminLogin);
+adminRouter.post("/signup", handleAdminSignup);   //_DONE_
+adminRouter.post("/login", handleAdminLogin);      //_DONE_
 
 adminRouter.use((req, res, next)=>{
     authenticate(req, res, next, jwt);
 })
 
-adminRouter.get("/dashboard",handleAdminDashboard);
-adminRouter.post("/addCourse", handleAdminCourseAdd);
-adminRouter.delete("/deleteCourse/:courseName", handleAdminCourseDelete);
-adminRouter.get("/myCourses", handleAdminCourses);
+adminRouter.get("/dashboard",handleAdminDashboard);           //_DONE_
+adminRouter.post("/addCourse", handleAdminCourseAdd);          //_DONE_
+
+adminRouter.delete("/deleteCourse", handleAdminCourseDelete);   //hardDelete                  : _DONE_
+adminRouter.patch("/deleteCourse", handleAdminCourseSTATUS);    //softDelete OR Disable       : _DONE_
+
+adminRouter.get("/myCourses", handleAdminCourses);                             // _DONE_
+adminRouter.get("/myCourses/content", handleAdminCourseContent);                // _DONE_
 
 
 //admin will need to hit an endpoint where he can add content to the course he added
-adminRouter.use("/courseUpdate",courseRouter);
+adminRouter.use("/courseUpdate",courseRouter);           
 
 
 
